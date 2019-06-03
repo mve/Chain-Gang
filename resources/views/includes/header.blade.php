f859bef4effbb3fb87ddcd06215ab391eeab6a06
Job <job.jansen@student.graafschapcollege.nl>
2019-06-03 13:03:42 +0200

    header with dropdown

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,44 @@
 <header>
 <div class="container-fluid p-2 m-0 d-flex align-items-end justify-content-end boven-balk">
-<i class="far fa-2x fa-id-card mr-4 text-white"></i> <i class="fas fa-2x fa-shopping-cart mr-4 text-white"></i>
+
+    <div class="dropdown">
+        <button class="btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+        <i class="far fa-2x fa-id-card mr-4 text-white"></i>
+        </button>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                    {{ __('My Profile') }}
+                </a>
+
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+    </div>
+
+
+    <div class="dropdown">
+        <a class="btn" href="cart">
+            <i class="fas fa-2x fa-shopping-cart mr-4 text-white"></i>
+        </a>
+
+    </div>
 </div>
 <div class="container-fluid p-0 m-0 header-image d-flex align-items-center justify-content-center">
     <img class="header-logo" src="images/template/logo.png">
 </div>
 <nav class="navbar navbar-expand-lg">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
     <i class="fa fa-bars text-white"></i>
     </button>
 
