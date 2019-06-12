be4793090901c6a50b0c5e359a21f570c87d43e4
Martvse <39297956+Martvse@users.noreply.github.com>
2019-06-12 11:48:28 +0200

    Product pagina
    Sterren verwijderd, reviews toegevoegd

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -22,11 +22,39 @@
             <div class="col-md-6 p-3 p-md-5">
                 <h4 class="mb-4 product-title">Schitterende fiets 01</h4>
                 <p>Blabla wat een schitterende omschrijving dus hier gaan we een heleboel mooie product gegevens neerknallen en wat informatie verschaffen maargoed heb nu echt genoeg getypt en heb er geen zin meer in. Doei.</p>
-                <h6 class="mb-2 mt-4 product-title">Product beoordeling</h6>
-                <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>
                 <div class="text-right">
                         <input type="number" min="1" max="9" step="1" value="1">
-                        <a href="#" class="btn btn-primary">Toevoegen <i class="fas fa-cart-plus"></i></a>
+                        <a href="#" class="btn btn-primary ml-1">Toevoegen <i class="fas fa-cart-plus"></i></a>
+                </div>
+            </div>
+            <div class="col-md-12 p-3 p-md-5">
+                <div class="row">
+                    <div class="col-md-6">
+                            <h4 class="mb-2 product-title">Reviews</h4>
+                                <!--- dit loopen --->
+                                <div class="review py-2">
+                                    <q>Review tekst hier dusja hier moeten we dat laten zien man wat ben ik slecht in placeholder tekst verzinnen pfoew, ik word nu wel moe in de vingers hoor. Ciao.</q>
+                                    <p class="review-author mt-1">- Mart Vrieze</p>
+                                </div>
+                                 <div class="review py-2">
+                                    <q>Review tekst hier dusja hier moeten we dat laten zien man wat ben ik slecht in placeholder tekst verzinnen pfoew, ik word nu wel moe in de vingers hoor. Ciao.</q>
+                                    <p class="review-author mt-1">- Mart Vrieze</p>
+                                </div>
+                                 <div class="review py-2">
+                                    <q>Review tekst hier dusja hier moeten we dat laten zien man wat ben ik slecht in placeholder tekst verzinnen pfoew, ik word nu wel moe in de vingers hoor. Ciao.</q>
+                                    <p class="review-author mt-1">- Mart Vrieze</p>
+                                </div>
+                                <!---- einde loopen --->
+                    </div>
+                    <div class="col-md-6">
+                            <h4 class="mb-2 product-title">Schrijf een review</h4>
+                            <!----- review formulier hier ----->
+
+
+
+
+                            <!----- einde review formulier ----->
+                    </div>
                 </div>
             </div>
         </div>
