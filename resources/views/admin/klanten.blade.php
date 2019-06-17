dfcaaeaac7dcfeb95e842fd218163c234d4da658
Mike van Egmond <mikevanegmond1998@outlook.com>
2019-06-17 13:49:25 +0200

    admin user page added.
    admin user page is added, there is no styling.

--- resources/views/admin/klanten.blade.php
+++ resources/views/admin/klanten.blade.php
@@ -2,10 +2,25 @@
 
 @section('content')
 
-<div class="row no-gutters justify-content-center">
-    <div class="col-12 text-center py-3" id="pagina-titel">
-        <h3 class="m-0">ChainGang - Klanten</h3>
+    <div class="row no-gutters justify-content-center">
+        <div class="col-12 text-center py-3" id="pagina-titel">
+            <h3 class="m-0">ChainGang - Klanten</h3>
+        </div>
+
+        <div class="container">
+            <div class="row">
+
+                @foreach($users as $user)
+
+                    <div style="text-align: center" class="col-md-12">
+                        <p>{{$user->name . " " . $user->lastname . " " . $user->email}}</p>
+                    </div>
+
+                @endforeach
+
+            </div>
+        </div>
+
     </div>
-</div>
 
 @endsection
--- resources/views/includes/sidebar_admin.blade.php
+++ resources/views/includes/sidebar_admin.blade.php
@@ -64,7 +64,7 @@
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="klanten">
                     <i class="fas fa-users"></i>
                     Customers
                 </a>
