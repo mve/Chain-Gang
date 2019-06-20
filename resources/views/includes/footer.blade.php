9aa3a5fc4c8b2f1fd334a995b8bfa6715bd64bc2
Martvse <39297956+Martvse@users.noreply.github.com>
2019-06-20 15:19:40 +0200

    Footer is now done
    - Please make footer functional else I will fuck off

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,42 +2,28 @@
     <div class="copyright-balk d-flex justify-content-center align-items-center">&copy; ChainGang 2019</div>
     <div class="footer-content">
         <div class="container h-100 py-3">
-            <div class="row h-100 align-items-center">
-                <div class="col-md-3 text-left">
-                    <div class="footer-item-header text-center w-100"><h5>Mijn account</h5></div>
-                    <ul>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
+            <div class="row h-100">
+                <div class="col-md-3 text-left my-4 my-sm-0">
+                    <div class="footer-item-header w-100 mb-3"><h5 class="footer-title">Mijn account</h5></div>
+                    <ul class="pl-0">
+                        <li><a href="#">Winkelwagen</a></li>
+                        <li><a href="#">Account details</a></li>
+                        <li><a href="#">Uitloggen</a></li>
                     </ul>
                 </div>
-                <div class="col-md-3 text-left">
-                    <div class="footer-item-header text-center w-100"><h5>Winkelwagen</h5></div>
-                    <ul>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
+                <div class="col-md-3 text-left mb-4 mb-sm-0">
+                    <div class="footer-item-header w-100 mb-3"><h5 class="footer-title">Fietsen</h5></div>
+                    <ul class="pl-0">
+                        <li><a href="#">Dames fietsen</a></li>
+                        <li><a href="#">Heren fietsem</a></li>
                     </ul>
                 </div>
-                <div class="col-md-3 text-left">
-                    <div class="footer-item-header text-center w-100"><h5>Test item</h5></div>
-                    <ul>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 text-left">
-                    <div class="footer-item-header text-center w-100"><h5>Test item</h5></div>
-                    <ul>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                        <li><a href="#">Test link</a></li>
-                    </ul>
+                <div class="col-md-6 text-left mb-4 mb-sm-0">
+                    <div class="footer-item-header w-100 mb-3"><h5 class="footer-title">Nieuwsbrief</h5></div>
+                    <p class="my-1">Schrijf je in voor onze nieuwsbrief!</p>
+                    <form>
+                        <input id="text" placeholder="jouwemail@phpscrumisleukkuchkuck.nl" name="text" type="text" class="form-control w-50 d-inline-block">  <button name="submit" type="submit" class="btn btn-primary">Inschrijven</button>
+                    </form>
                 </div>
             </div>
         </div>
