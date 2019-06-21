8cbb6bdc96b5fff7e9ad1a1dc745d99a656f22c9
Mike van Egmond <mikevanegmond1998@outlook.com>
2019-06-21 09:53:32 +0200

    added links to the backend page.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,66 +2,68 @@
 
 @section('content')
 
-<div class="row no-gutters justify-content-center">
-    <div class="col-12 text-center py-3" id="pagina-titel">
-        <h3 class="m-0">ChainGang - Dashboard</h3>
-    </div>
-    <div class="col-md-3 mt-4 mt-sm-4 mt-md-4" id="recente-orders">
+    <div class="row no-gutters justify-content-center">
+        <div class="col-12 text-center py-3" id="pagina-titel">
+            <h3 class="m-0">ChainGang - Dashboard</h3>
+        </div>
+        <div class="col-md-3 mt-4 mt-sm-4 mt-md-4" id="recente-orders">
             <div class="card h-100 w-100">
-        <div class="card-header">
-            Recente orders
+                <div class="card-header">
+                    Recente orders
+                </div>
+
+                <ul class="list-group list-group-flush">
+                    @foreach($orders->reverse()->take(5) as $order)
+                        <li class="list-group-item"><a href="#">#{{$order->id}}</a></li>
+                    @endforeach
+                </ul>
+
+            </div>
         </div>
-        <ul class="list-group list-group-flush">
-                @foreach($orders->reverse()->take(5) as $order)
-                <li class="list-group-item"><a href="#"">#{{$order->id}}</a></li>
-                @endforeach
-        </ul>
-        </div>
-    </div>
-    <div class="col-md-7 offset-0 offset-sm-0 offset-md-1 mt-4 mt-sm-4 mt-md-4" id="omzet-grafiek">
+        <div class="col-md-7 offset-0 offset-sm-0 offset-md-1 mt-4 mt-sm-4 mt-md-4" id="omzet-grafiek">
             <div class="card h-100 w-100">
-        <div class="card-header">
-            Omzet
+                <div class="card-header">
+                    Omzet
+                </div>
+                <canvas id="canvas" class="chartjs-render-monitor omzet-grafiek"></canvas>
+            </div>
         </div>
-        <canvas id="canvas" class="chartjs-render-monitor omzet-grafiek"></canvas>
-        </div>
-    </div>
-    <div class="col-md-3 mt-4 mt-sm-4 mt-md-4">
+        <div class="col-md-3 mt-4 mt-sm-4 mt-md-4">
             <div class="card h-100 w-100">
-        <div class="card-header">
-            Nieuwste klanten
+                <div class="card-header">
+                    Nieuwste klanten
+                </div>
+                <ul class="list-group list-group-flush">
+                    @foreach($users->reverse()->take(5) as $user)
+                        <li class="list-group-item"><a href="/admin/klanten/{{ $user->id }}">{{$user->name . " " . $user->lastname}}</a></li>
+                    @endforeach
+                </ul>
+            </div>
         </div>
-        <ul class="list-group list-group-flush">
-                @foreach($users->reverse()->take(5) as $user)
-                    <li class="list-group-item"><a href="#"">{{$user->name . " " . $user->lastname}}</a></li>
-                @endforeach
-        </ul>
-        </div>
-    </div>
-    <div class="col-md-3 offset-0 offset-sm-0 offset-md-1 mt-4 mt-sm-4 mt-md-4">
+        <div class="col-md-3 offset-0 offset-sm-0 offset-md-1 mt-4 mt-sm-4 mt-md-4">
             <div class="card h-100 w-100">
-        <div class="card-header">
-            Recente orders
+                <div class="card-header">
+                    Recente orders
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+            </div>
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-        </div>
-    </div>
-    <div class="col-md-3 offset-0 offset-sm-0 offset-md-1 mt-4 mt-sm-4 mt-md-4">
+        <div class="col-md-3 offset-0 offset-sm-0 offset-md-1 mt-4 mt-sm-4 mt-md-4">
             <div class="card h-100 w-100">
-        <div class="card-header">
-            Recente orders
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
+                <div class="card-header">
+                    Recente orders
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Cras justo odio</li>
+                    <li class="list-group-item">Dapibus ac facilisis in</li>
+                    <li class="list-group-item">Vestibulum at eros</li>
+                </ul>
+            </div>
         </div>
     </div>
-</div>
 
 @endsection
--- resources/views/admin/klanten.blade.php
+++ resources/views/admin/klanten.blade.php
@@ -7,39 +7,34 @@
             <h3 class="m-0">ChainGang - Klanten</h3>
         </div>
 
-            <div class="col-12">
+        <div class="col-12">
 
 
-                <table class="display nowrap w-100" id="data-table">
-                    <thead>
-                        <tr>
-                        <th>Klant ID</th>
-                        <th>Klant naam</th>
-                        <th>Email adres</th>
-                        <th>Plaats</th>
-                        <th>Acties</th>
-                        </tr>
-                        </thead>
-                            <tbody>
+            <table class="display nowrap w-100" id="data-table">
+                <thead>
+                <tr>
+                    <th>Klant ID</th>
+                    <th>Klant naam</th>
+                    <th>Email adres</th>
+                    <th>Plaats</th>
+                    <th>Acties</th>
+                </tr>
+                </thead>
+                <tbody>
 
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name . " " . $user->lastname}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->city}}</td>
+                        <td class="text-center"><a href="/admin/klanten/{{$user->id}}" class="btn btn-primary">Bewerken <i class="fas fa-edit"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
 
-                            @foreach($users as $user)
-                            <tr>
-                                <a href="/admin/klanten/{{ $user->id }}">
-                                    {{ $user->name }}
-                                </a>
-
-                                <td>{{$user->id}}</td>
-                                <td>{{$user->name . " " . $user->lastname}}</td>
-                                <td>{{$user->email}}</td>
-                                <td>{{$user->city}}</td>
-                                <td class="text-center"><a href="#" class="btn btn-primary">Bewerken <i class="fas fa-edit"></i></a></td>
-                            </tr>
-                            @endforeach
-
-                            </tbody>
-                    </table>
-
+                </tbody>
+            </table>
 
 
         </div>
